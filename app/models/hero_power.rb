0791558853a412aeedf2760022feada2411a06be
Verah Kwamboka <verahnyagaka101@gmail.com>
2022-10-08 17:40:16 +0300

    added validations

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,11 @@
 class HeroPower < ApplicationRecord
    belongs_to :hero
    belongs_to :power
+
+   #validate strength
+   STRENGTH = [ 'Strong', 'Weak', 'Average']
+   validates :strength, inclusion:{
+    in: STRENGTH,
+    message: `must be one of: #{STRENGTH.join(',')}`
+}
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -1,4 +1,7 @@
 class Power < ApplicationRecord
     has_many :hero_powers
     has_many :heroes, through: :hero_powers
+
+    # description validation
+    validates :description, presence: true, length: {minimum:20}
 end

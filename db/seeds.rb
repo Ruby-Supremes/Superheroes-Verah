99d741290d692adcbc6a8a3c71e9be62732259d3
Verah Kwamboka <verahnyagaka101@gmail.com>
2022-10-08 17:21:57 +0300

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
+puts "seeding"
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+heroes = Hero.create([
+    {name: "vee", super_name: "Hero V"},
+    {name: "Robi", super_name: "Hero R"},
+    {name: "Shi", super_name: "Hero S"},
+    {name: "mom", super_name: "Hero M"}
+    ])
+
+powers = Power.create([
+    { name: "super speed", description: "very fast"},
+    { name: "telepotation", description: "very fast"},
+    { name: "invisibility", description: "invisible"}
+    ])
+
+heropowers = HeroPower.create([
+    {strength: "Strong", hero_id: 1,power_id: 1},
+    {strength: "Strong", hero_id: 2,power_id: 1},
+    {strength: "Average", hero_id: 3,power_id: 2},
+    {strength: "Weak", hero_id: 4,power_id: 3}
+    ])
+puts "seeded"
